b606e716d1edcca9b0a750307b36be8a79a598b7
Schurkino <alexej.schurkin@gmail.com>
2026-07-30 01:32:03 +0200

    Update and rename script_snow.js to script_particle.js

--- script_particle.js
+++ script_particle.js
@@ -1,29 +1,31 @@
-function createSnowflake() {
-    const snowContainer = document.getElementById("js_snow");
-    const snowContainerClone = document.getElementById("js_snow_clone");
+function createParticle() {
+    const particleContainer = document.getElementById("js_particle");
+    const particleContainerClone = document.getElementById("js_particle_clone");
 
     /* Flocke für den originalen Container */
-    if (snowContainer) {
-        const flake = document.createElement("div");
-        flake.classList.add("snowflake");
+    if (particleContainer) {
+        const particle = document.createElement("div");
+        particle.classList.add("particle");
 
-        flake.style.left = Math.random() * 100 + "%";
+        particle.style.left = (Math.random() * 125 - 25) + "%";
+        particle.style.top = (Math.random() * -100) + "%";
+        particle.style.zIndex = Math.floor(Math.random() * 8);
 
         /* Math.random() erzeugt eine Kommazahl zwischen 0 und 1.
         Multipliziert mit 4 ergibt das einen Wert zwischen 0 und 4.
-        Durch das Addieren von 8 liegt das Endergebnis der Flugdauer
-        am Ende variabel zwischen minimal 8 und maximal 12 Sekunden. */
+        Durch das Addieren von 6 liegt das Endergebnis der Flugdauer
+        am Ende variabel zwischen minimal 6 und maximal 10 Sekunden. */
         const duration = Math.random() * 4 + 6; 
 
         /* Die berechnete Zahl wird mit der CSS-Zeiteinheit 's' (Sekunden)
         verknüpft und direkt als Inline-Style für die CSS-Animation
-        auf der geklonten Schneeflocke hinterlegt. */
-        flake.style.animationDuration = duration + "s";
+        auf der Schneeflocke hinterlegt. */
+        particle.style.animationDuration = duration + "s";
 
         /* Das fertig konfigurierte Flocken-Element wird nun als echtes
-        Kindelement in den geklonten Schnee-Container eingehängt,
+        Kindelement in den Schnee-Container eingehängt,
         wodurch die CSS-Animation im Browser sofort startet. */
-        snowContainer.appendChild(flake);
+        particleContainer.appendChild(particle);
 
         /* Um den Arbeitsspeicher zu schonen und das DOM nicht mit alten,
         unsichtbaren Elementen zu überlasten, wird ein Timer gestartet.
@@ -31,32 +33,34 @@ function createSnowflake() {
         Flugdauer in Sekunden mit 1000. Sobald diese Zeit abgelaufen ist,
         wird die Flocke restlos aus dem DOM gelöscht. */
         setTimeout(function() {
-            flake.remove();
+            particle.remove();
         }, duration * 1000);
     }
 
     /* Flocke für den geklonten Container */
-    if (snowContainerClone) {
-        const cloneFlake = document.createElement("div");
-        cloneFlake.classList.add("snowflake");
+    if (particleContainerClone) {
+        const particleClone = document.createElement("div");
+        particleClone.classList.add("snowflake");
 
-        cloneFlake.style.left = Math.random() * 100 + "%";
+        particleClone.style.left = (Math.random() * 125 - 25) + "%";
+        particleClone.style.top = (Math.random() * -100) + "%";
+        particleClone.style.zIndex = Math.floor(Math.random() * 8);
       
         /* Math.random() erzeugt eine Kommazahl zwischen 0 und 1.
         Multipliziert mit 4 ergibt das einen Wert zwischen 0 und 4.
-        Durch das Addieren von 8 liegt das Endergebnis der Flugdauer
-        am Ende variabel zwischen minimal 8 und maximal 12 Sekunden. */
+        Durch das Addieren von 6 liegt das Endergebnis der Flugdauer
+        am Ende variabel zwischen minimal 6 und maximal 10 Sekunden. */
         const durationClone = Math.random() * 4 + 6;
         
         /* Die berechnete Zahl wird mit der CSS-Zeiteinheit 's' (Sekunden)
         verknüpft und direkt als Inline-Style für die CSS-Animation
         auf der geklonten Schneeflocke hinterlegt. */
-        cloneFlake.style.animationDuration = durationClone + "s";
+        particleClone.style.animationDuration = durationClone + "s";
 
         /* Das fertig konfigurierte Flocken-Element wird nun als echtes
         Kindelement in den geklonten Schnee-Container eingehängt,
         wodurch die CSS-Animation im Browser sofort startet. */
-        snowContainerClone.appendChild(cloneFlake);
+        particleContainerClone.appendChild(particleClone);
 
         /* Um den Arbeitsspeicher zu schonen und das DOM nicht mit alten,
         unsichtbaren Elementen zu überlasten, wird ein Timer gestartet.
@@ -64,10 +68,10 @@ function createSnowflake() {
         Flugdauer in Sekunden mit 1000. Sobald diese Zeit abgelaufen ist,
         wird die Flocke restlos aus dem DOM gelöscht. */
         setTimeout(function() {
-            cloneFlake.remove();
+            particleClone.remove();
         }, durationClone * 1000);
     }
 }
 
 /* Intervall vergrößert, damit die Flocken nacheinander starten */
-setInterval(createSnowflake, 400);
+setInterval(createParticle, 50);
